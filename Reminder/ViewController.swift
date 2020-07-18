512ef818bd16a706fd377bf2c893b12fd33990d3
Vlad <vecherkovsky2003@mail.ru>
2020-07-18 14:40:21 +0300

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by Владислав Вечерковский on 18.07.2020.
+//  Copyright © 2020 Владислав Вечерковский. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
